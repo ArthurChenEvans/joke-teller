3bb426216389d18984b6017fec2fbbb2dc939d95
ArthurChenEvans <arthurchenevans@protonmail.com>
2024-03-16 22:27:43 +0000

    Created random joke generator. Removed audio because API was not working

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 'use strict';
 
 const apiURL = 'https://official-joke-api.appspot.com/jokes/programming/random';
+const jokeBtn = document.getElementById('button');
+const textArea = document.getElementById('text-area');
 
+const jokeClass = function(setup, punchline)
+{
+    this.setup = setup;
+    this.punchline = punchline;
+}
+
+const getJoke = async function() {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    const joke = data[0];
+    return new jokeClass(joke.setup, joke.punchline);
+}
+
+jokeBtn.addEventListener('click', () => {
+    getJoke().then(joke => {
+        textArea.textContent = `${joke.setup} \n\n ${joke.punchline}`
+        }
+    )
+})
